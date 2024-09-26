def194e58bbe5d53f1c5f8cde3154b21cdc4fb53
stevenpy <stevenphilbert@gmail.com>
2024-09-26 20:04:15 +0200

    Sharing works, add create method

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -4,10 +4,16 @@ class SharesController < ApplicationController
 
   def new
     @users = User.excluding(current_user)
-    
+    @user_password = UserPassword.new
   end
 
   def create
+    @user_password = @password.user_passwords.new(user_password_params)
+    if @user_password.save
+      redirect_to @password
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -17,5 +23,9 @@ class SharesController < ApplicationController
 
   def set_password
     @password = current_user.passwords.find(params[:password_id])
+  end
+
+  def user_password_params
+    params.require(:user_password).permit(:user_id)
   end
 end
