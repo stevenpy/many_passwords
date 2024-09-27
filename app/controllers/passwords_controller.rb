cdd0553602b6e64c6625d1e5d891cff2eb4b0a88
stevenpy <stevenphilbert@gmail.com>
2024-09-27 16:57:46 +0200

    loop through join table in view

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -26,7 +26,7 @@ class PasswordsController < ApplicationController
 
   def create
     @password = Password.new(password_params)
-    @password.user_passwords(user: current_user, role: :owner)
+    @password.user_passwords.new(user: current_user, role: :owner)
     if @password.save
       redirect_to @password
     else
